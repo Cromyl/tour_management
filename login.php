f0a636fb55e3c2badc08d92c4ab68e66267ee4bf
Radhika <radhikag970@gmail.com>
2023-02-13 21:00:20 +0530

    Added login.php

--- login.php
+++ login.php
@@ -5,18 +5,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Login</h1>
+    <div class="bgimg"></div>
+    <div class="bgtext">
 
-    <form action="connect2.php" method="POST">
-        <label for="name">User Name :</label> <br>
-        <input type="text" name='name' id='name' required> <br><br>
+        <h1>Login</h1>
+         
+        <form action="connect2.php" method="POST">
+            <label for="name">User Name :</label> <br>
+            <input type="text" name='name' id='name' required placeholder="Enter Your Name"> <br><br>
+    
+            <label for="password">Password :</label> <br>
+            <input type="password" name='password' id='password' required placeholder="Enter Your Password"> <br><br>
+    
+            <input type="submit" name="submit1" id="submit1">
+        </form>
+        
 
-        <label for="password">Password :</label> <br>
-        <input type="password" name='password' id='password' required> <br><br>
-
-        <input type="submit" name="submit1" id="submit1">
-    </form>
+    </div>
 </body>
 </html>
